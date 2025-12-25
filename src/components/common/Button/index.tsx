b43db02624c361db4052ae173e12ad956a2db72c
thisme8 <stutiupreti8@gmail.com>
2025-12-25 15:02:20 +0545

    feat(sidebar-component) : Create a common component for the sidebar

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -3,7 +3,8 @@ import { cn } from '@/lib/utils';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 export const buttonVariants = cva(
-"inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",{
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
+  {
     variants: {
       variant: {
         default: 'border-2 border-secondary-100 text-sm text-white font-bold rounded-lg w-fit',
@@ -11,8 +12,8 @@ export const buttonVariants = cva(
           'bg-undraw-secondary-100 border-2 border-secondary-100 rounded-3xl flex flex-row gap-3 h-12 text-white font-semibold px-2 place-self-end',
         ghost: 'bg-transparent text-placeholder-100',
         icon: 'bg-transparent p-0 m-0',
-        navigation:'bg-tertiary-100 border-2 border-secondary-100 rounded-xl flex flex-row gap-3 h-12 w-40 text-white font-semibold px-2 place-self-end',
-        
+        navigation:
+          'bg-tertiary-100 border-2 border-secondary-100 rounded-md shadow-sm shadow-primary-100 text-md flex flex-row items-center gap-3 h-12 w-50 text-white font-semibold px-2 place-self-end',
       },
       size: {
         default: 'px-10 py-2',
--- src/components/common/SideBar/index.tsx
+++ src/components/common/SideBar/index.tsx
@@ -1,25 +1,52 @@
 'use client';
 
-import { Button } from "../Button";
+import Link from 'next/link';
+import { Icon } from '@iconify/react';
+import { usePathname } from 'next/navigation';
 
 export const SideBar = () => {
+  const pathname = usePathname();
+
+  const links = [
+    { href: '/directory', icon: 'bxs:food-menu', label: 'Directory' },
+    { href: '/rides', icon: 'mdi:bike-fast', label: 'View Rides' },
+    { href: '/trips', icon: 'mdi:account-pending', label: 'View Pending Trips' },
+    { href: '/about-us', icon: 'ix:about', label: 'About Us' },
+  ];
 
   return (
-    <div className="absolute bottom-10 h-[80vh] w-[35vh] mx-8 rounded-lg bg-card-bg-100 shadow-lg shadow-gray-800 place-content-start flex flex-col gap-5 items-center justify-items-stretch text-undraw-secondary-100">
-      
-      <h2 className="text-text-one-100 ">
-        Welcome to Milera
-      </h2>
-      
-      <Button
-      variant={'navigation'}
-      >Directory</Button>
-      <Button
-      variant={'navigation'}
-      >View Rides</Button>
-      <Button
-      variant={'navigation'}
-      >Your Pending Trips</Button>
+    <div className="absolute bottom-10 h-[80vh] w-[35vh] mx-8 rounded-lg bg-card-bg-100 shadow-lg shadow-gray-900 flex flex-col justify-between text-text-two-100 px-4 py-6">
+      {/* Top Section */}
+      <section className="flex flex-col gap-4">
+        <h2 className="text-text-one-100/80 font-semibold text-xl text-center border-b border-tertiary-100/20 w-full pb-2 mb-4">
+          Features
+        </h2>
+
+        {links.map((item) => {
+          const isActive = pathname === item.href;
+          return (
+            <Link
+              key={item.href}
+              href={item.href}
+              className={`group w-full flex items-center gap-4 rounded-md py-3 px-4 transition-all duration-200
+                ${
+                  isActive
+                    ? 'bg-tertiary-100/30 text-text-one-100/80 scale-98'
+                    : 'text-tertiary-100 hover:rounded-md hover:bg-tertiary-100/30 hover:text-text-one-100/80 hover:scale-98'
+                }
+              `}
+            >
+              <Icon icon={item.icon} width={24} height={24} className='text-shadow-2xs'/>
+              <span className="text-md font-medium">{item.label}</span>
+            </Link>
+          );
+        })}
+      </section>
+
+      <section className="h-40 w-[85%] bg-radial-[at_25%_25%] from-bg-card-bg-100 to-primary-100 shadow-md shadow-gray-900  mx-auto my-5 rounded-md flex flex-col gap-3 items-center justify-center text-tertiary-100 text-sm border-black">
+      <div className='flex flex-row gap-2 items-center text-text-one-100/80'><Icon icon='mdi:robot-happy' width={24} height={24}/><span>58 Happy Outsiders</span></div>
+      <p className='font-semibold text-md'>@Outside Shares</p>
+      </section>
     </div>
   );
 };
